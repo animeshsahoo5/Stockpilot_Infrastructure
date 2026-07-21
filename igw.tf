97c51b474283e18c06c92f9af75d298da4ae46bc
animeshsahoo5 <arnapurnasahoo79@gmail.com>
2026-07-21 17:28:41 +0530

    intial vpc configuration

---
+++ igw.tf
@@ -0,0 +1,10 @@
+resource "aws_internet_gateway" "main" {
+
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-igw"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
---
+++ subnet.tf
@@ -0,0 +1,58 @@
+resource "aws_subnet" "public_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_1_cidr
+
+  availability_zone = "ap-south-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-subnet-1"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+resource "aws_subnet" "public_2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_2_cidr
+
+  availability_zone = "ap-south-1b"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-subnet-2"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+resource "aws_subnet" "private_subnet_1" {
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_1_cidr
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-private-subnet-1"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+resource "aws_subnet" "private_subnet_2" {
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_2_cidr
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-private-subnet-2"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
